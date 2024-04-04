a5f388f0a805f3dc1a49295fe231e836d02d6332
Alok Sharma <aloksharma10969@gmail.com>
2024-04-04 10:57:29 +0530

    Outside click disable in modal

--- components/modal/add-pickup-location.tsx
+++ components/modal/add-pickup-location.tsx
@@ -131,7 +131,7 @@ export const AddPickupLocationModal = () => {
 
     return (
         <Dialog open={isModalOpen} onOpenChange={handleClose}>
-            <DialogContent className="bg-white dark:text-white text-black p-0 overflow-hidden max-w-2xl">
+            <DialogContent className="bg-white dark:text-white text-black p-0 max-w-2xl">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl font-bold">
                         Add Pickup Location
