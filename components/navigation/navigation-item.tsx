206a26ff226cacae336ae03df3b76e816ec61403
aloksharma10 <aloksharma10969@gmail.com>
2024-05-31 20:14:36 +0530

    rev order buckting button added

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/navigation-menu"
 import { usePathname, useSearchParams } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
 
 export function NavigationItem({ links }: { links: { label: string; href: string }[] }) {
     const pathname = usePathname()
@@ -30,6 +31,31 @@ export function NavigationItem({ links }: { links: { label: string; href: string
                                     {link.label}
                                 </NavigationMenuLink>
                             </Link>
+                        </NavigationMenuItem>
+                    );
+                })}
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
+
+export function RevOrderFilter({ links, handleRevFilter }: { links: { label: string; bucket: number }[], handleRevFilter: (bucket: number | undefined) => void }) {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                {links.map((link) => {
+                    // const isActive = status ? link.href === `/orders?status=${status}` : link.href === pathname;
+                    return (
+                        <NavigationMenuItem key={link.label}>
+                            <button onClick={() => { handleRevFilter(link.bucket) }}>
+                                <NavigationMenuLink
+                                    className={cn("group inline-flex h-10 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-slate-100 hover:text-slate-900",
+                                        // isActive ? "text-red-500 font-medium bg-gray-100 shadow-sm" : ""
+                                    )}
+                                >
+                                    {link.label}
+                                </NavigationMenuLink>
+                            </button>
                         </NavigationMenuItem>
                     );
                 })}
