8b5b8c1416bf5e2bb0019bc880a97928327840bb
Kashish <kashish.biserwal@protonmail.com>
2024-08-17 16:03:29 +0530

    zoho loading while invoice generation

--- app/(admin)/admin/users/users-list/page.tsx
+++ app/(admin)/admin/users/users-list/page.tsx
@@ -5,20 +5,25 @@ import { useAxios } from "@/components/providers/AxiosProvider";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
 
 export default function UsersListing() {
+    const [loading, setLoading] = useState(false);
     const {axiosIWAuth} = useAxios();
     const {userToken} = useAuth();
     const { toast } = useToast();
 
     async function handleGenerateInvoices() {
+        setLoading(true);
         const res = await axiosIWAuth.get(`/admin/generate-invoice`);
         if (res.data?.valid) {
+            setLoading(false);
             toast({
                 variant: "default",
                 title: "Invoices Generated Successfully",
               });
         }else{
+            setLoading(false);
             toast({
                 variant: "destructive",
                 title: "Failed to Generate Invoices",
@@ -31,6 +36,7 @@ export default function UsersListing() {
             <CardHeader>
                 <CardTitle className="md:flex justify-between space-y-3">
                    Users Listing
+                    {loading && <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-900"></div>}
                     <Button variant={"themeButton"} onClick={handleGenerateInvoices}>Generate Invoices</Button>
                 </CardTitle>
                 <div className="flex justify-end">
