22d9d7817dc4c78cd65a5067a4ef773b0a89556b
aloksharma10 <aloksharma10969@gmail.com>
2024-08-22 15:58:25 +0530

    Fixes

--- app/(seller)/settings/page.tsx
+++ app/(seller)/settings/page.tsx
@@ -2,13 +2,14 @@ import Link from 'next/link';
 import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
 import { ChevronRight } from 'lucide-react';
-import Workflow from '@/components/SVGs/channel.svg'
-import FIcon from '@/components/SVGs/FIcon.svg'
+import Workflow from '@/components/SVGs/channel.svg';
+import FIcon from '@/components/SVGs/file.svg';
 
-import CarPNG from '@/components/SVGs/car.png'
-import CompanyPNG from '@/components/SVGs/company.png'
-import PrinterPNG from '@/components/SVGs/printer.png'
-import UserPNG from '@/components/SVGs/user.png'
+import CarPNG from '@/components/SVGs/car.png';
+import CompanyPNG from '@/components/SVGs/company.png';
+import PrinterPNG from '@/components/SVGs/printer.png';
+import UserPNG from '@/components/SVGs/user.png';
+
 interface LinkItem {
   href: string;
   label: string;
@@ -17,24 +18,24 @@ interface LinkItem {
 interface SettingsCardProps {
   title: string;
   links: LinkItem[];
-  logo: string;
+  logo: any;
 }
 
 const SettingsCard: React.FC<SettingsCardProps> = ({ title, links, logo }) => (
   <Card className="drop-shadow-md p-1 rounded-2xl">
-    <CardContent className='pt-4 h-[187px]'>
-      <div className='flex'>
+    <CardContent className="pt-4 h-[187px]">
+      <div className="flex">
         <div className="mr-3">
           <Image src={logo} alt="Settings Icon" width={30} height={30} />
         </div>
-        <div className='grid place-content-center'>
+        <div className="grid place-content-center">
           <h3 className="font-medium text-lg">{title}</h3>
         </div>
       </div>
       <div className="grid mx-12 gap-1 my-3">
         {links.map(({ href, label }, index) => (
           <Link key={index} href={href}>
-            <div className='flex justify-between text-sm hover:text-gray-500'>
+            <div className="flex justify-between text-sm hover:text-gray-500">
               <p>{label}</p>
               <ChevronRight size={18} />
             </div>
@@ -52,45 +53,42 @@ const Settings = () => {
       links: [
         { href: '/settings/company/company-profile', label: 'Company Profile' },
         { href: '/settings/company/kyc', label: 'KYC' },
-        { href: '/settings/company/change-password', label: 'Change Password' }
+        { href: '/settings/company/change-password', label: 'Change Password' },
       ],
-      logo: CompanyPNG
+      logo: CompanyPNG,
     },
     {
       title: 'COD Payments',
-      links: [
-        { href: '/settings/cod-payments/bank-details', label: 'Bank Details' },
-      ],
-      logo: UserPNG
+      links: [{ href: '/settings/cod-payments/bank-details', label: 'Bank Details' }],
+      logo: UserPNG,
     },
     {
       title: 'Billing',
       links: [
         { href: '/settings/billing/gstin-invoicing', label: 'GSTIN Invoicing' },
-        { href: '/settings/billing/billing-address', label: 'Billing Address' }
+        { href: '/settings/billing/billing-address', label: 'Billing Address' },
       ],
-      logo: PrinterPNG
+      logo: PrinterPNG,
     },
     {
       title: 'Pickup Address',
       links: [
-        { href: '/settings/pickup-address/manage-pickup-addresses', label: 'Manage Pickup Addresses' }
+        {
+          href: '/settings/pickup-address/manage-pickup-addresses',
+          label: 'Manage Pickup Addresses',
+        },
       ],
-      logo: CarPNG
+      logo: CarPNG,
     },
     {
       title: 'Couriers Price',
-      links: [
-        { href: '/settings/courier-price', label: 'Courier Price' }
-      ],
-      logo: Workflow
+      links: [{ href: '/settings/courier-price', label: 'Courier Price' }],
+      logo: Workflow,
     },
     {
       title: 'Channels',
-      links: [
-        { href: '/settings/manage-channels', label: 'Manage Channels' }
-      ],
-      logo: Workflow
+      links: [{ href: '/settings/manage-channels', label: 'Manage Channels' }],
+      logo: Workflow,
     },
     {
       title: 'Policies',
@@ -100,16 +98,16 @@ const Settings = () => {
         { href: '/settings/policies/shipment-and-delivery', label: 'Shipment and Delivery' },
         { href: '/settings/policies/terms-and-conditions', label: 'Terms and Conditions' },
       ],
-      logo: FIcon
-    }
+      logo: FIcon,
+    },
   ];
 
   return (
     <div>
-      <h1 className='py-5 text-2xl font-semibold'>Settings</h1>
-      <div className='grid grid-cols-3 gap-14 pb-5'>
+      <h1 className="py-5 text-2xl font-semibold">Settings</h1>
+      <div className="grid grid-cols-3 gap-14 pb-5">
         {settingsData.map((data, index) => (
-          <SettingsCard key={index} title={data.title} links={data.links} logo={data.logo.src} />
+          <SettingsCard key={index} title={data.title} links={data.links} logo={data.logo} />
         ))}
       </div>
     </div>
